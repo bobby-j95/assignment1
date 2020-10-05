7a074965aa477aa7e2fb2bf9ee3cca6894348164
Robert Johns <robertjohns08@gmail.com>
2020-10-05 01:01:23 -0400

    Project files with proper files from fork! All code was copied from previous repository found in important text file.

--- src/main/java/com/meritamerica/assignment1/MeritAmericaBankApp.java
+++ src/main/java/com/meritamerica/assignment1/MeritAmericaBankApp.java
@@ -1,5 +1,10 @@
 package com.meritamerica.assignment1;
 
+import java.util.Scanner;
+
 public class MeritAmericaBankApp {
-	
+	public static void main(String args[]) {
+		Scanner s=new Scanner(System.in);
+		
+	}
 }
